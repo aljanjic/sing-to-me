fc989810c5f601ffea087a8553d755bf00925a38
Aljosa <janjic_aljosa@hotmail.com>
2024-04-24 20:53:44 +0200

    Basic search and filtering capabilities

--- src/pages/api/songs.ts
+++ src/pages/api/songs.ts
@@ -1,14 +1,26 @@
+// pages/api/songs.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../lib/mongodb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const client = await clientPromise;
-    const db = client.db('sing-to-me');
-    
-    const songs = await db.collection('songs').find({}).toArray();
-    res.status(200).json(songs);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
+    try {
+        const { search, filter } = req.query;
+        const client = await clientPromise;
+        const db = client.db("sing-to-me");
+
+        let query = {};
+
+        if (search) {
+            query = { ...query, songName: { $regex: search, $options: "i" } };  // Case-insensitive regex search
+        }
+
+        if (filter) {
+            query = { ...query, genres: filter };
+        }
+
+        const songs = await db.collection("songs").find(query).toArray();
+        res.status(200).json(songs);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
 }
