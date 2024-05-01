f0c665be9fb124039ce5d374a8477b5631b94013
Aljosa <janjic_aljosa@hotmail.com>
2024-05-01 11:39:05 +0200

    Static Site Generation with getStaticProps

--- src/pages/songs/index.tsx
+++ src/pages/songs/index.tsx
@@ -1,7 +1,54 @@
-import { NextPage } from "next";
+import Song from "@/components/Song";
+import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 
-const Songs: NextPage = () => {
-    return <h1>Songs</h1>
+
+type Song = {
+    _id: string,
+    musician: string,
+    songName: string,
+    genres: string[]
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    return {
+        props: {
+            songs: [
+                {
+                    _id:"662a51452176c759f3ee1f",
+                    musician:"The Beatles",
+                    songName:"Come Together",
+                    genres:["Rock","Classic"]
+                },
+
+                {
+                    _id:"662a512q2176c759f3edfe20",
+                    musician:"The Beatles",
+                    songName:"Hey Jude",
+                    genres:["Rock"]
+                },
+            ] as Song[]
+        }
+    }
+};
+
+const Songs: NextPage = ({songs}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    console.log(songs)
+    return (
+        <>  
+            <h1> Songs: </h1>
+            {songs.map((song: Song) => {
+                return (
+                    <div>
+                    <p>{song.musician}</p>
+                    <p>{song.songName}</p>
+                    <p>{song.genres.map((genre)=>{
+                        return(`${genre} `)
+                    })}</p>
+                    </div>
+                )
+            }) }
+        </>
+    )
 }
 
 export default Songs
