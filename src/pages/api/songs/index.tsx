6ad739c3975fbd0f9294256bdc1b7e93712cc913
Aljosa <janjic_aljosa@hotmail.com>
2024-05-07 13:48:50 +0200

    Installing nexjs-cors and code refactor

--- src/pages/api/songs/index.tsx
+++ src/pages/api/songs/index.tsx
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "@/lib/mongodb";
 import { Song } from '../../songs/index'
 import { ObjectId } from "mongodb";
+import NextCors from "nextjs-cors";
 
 type Return = {
     songs:  Song[];
@@ -9,24 +10,40 @@ type Return = {
 
 export const getSongs = async(): Promise< Song[]> => {
     const mongoClient = await clientPromise;
-    const data = await mongoClient.db('sing-to-me').collection('songs').find().toArray() as Song[];
+
+    const data = await mongoClient
+            .db('sing-to-me')
+            .collection('songs')
+            .find().toArray() as Song[];
+
     return JSON.parse(JSON.stringify(data))
 }
 
 export const addSong = async(song: Song) : Promise <ObjectId> => {
     const mongoClient = await clientPromise;
-    const response = await mongoClient.db('sing-to-me').collection('songs').insertOne(song)
 
-    return response.insertedId
+    const response = await mongoClient
+        .db('sing-to-me')
+        .collection('songs')
+        .insertOne(song)
+
+    return response.insertedId;
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse <Return | ObjectId | {error: string} > ) => {
+export default async (
+    req: NextApiRequest,
+    res: NextApiResponse <Return | ObjectId | {error: string} >
+ ) => {
+    await NextCors(req, res, {
+        methods: ['GET', 'POST'],
+        origin: ['*'],
+        optionSuccessStatus: 200,
+    });
+
     if (req.method == 'GET') {
         const data = await getSongs();
         res.status(200).json({songs: data})
     } else if (req.method == 'POST') {
-        console.log(req.body)
-
         if(req.body.musician && req.body.songName && req.body.genres){
             const song: Song = {
                 musician: req.body.musician,
