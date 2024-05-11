251fe5ca0334a90fc9a59f673293b6ad72ff82d8
Aljosa <janjic_aljosa@hotmail.com>
2024-05-11 18:19:39 +0200

    Styling the song list

--- src/components/SongComponent.tsx
+++ src/components/SongComponent.tsx
@@ -15,20 +15,16 @@ const SongComponent = ({song} : {song: Song}) => {
         >
 
         {/* <Grid container direction="column" justifyContent="center" alignItems="center" > */}
-            <Paper elevation={3} style={{width: 300, padding:10, backgroundColor:'#adc5b7', }}> 
-            <h3>
-            <span
-                style={{
+            <Paper elevation={3} style={{width: 300, padding: 1, backgroundColor:'#adc5b7', }}> 
+            <h3 style={{
                     display: 'flex',
-                    alignItems: 'center',
-                    flexWrap: 'wrap'
-                }}
-                >
+                    flexDirection: 'column', // Set direction of flex items to column
+                    alignItems: 'center', // Center align items horizontally
+                    justifyContent: 'center' // Center align items vertically
+                }}>
             <span style={{fontStyle: 'italic', marginRight: 2 }}> {song.musician}</span> 
-            <span> { ' - ' } </span>
             <span style={{  marginLeft: 2}}> {song.songName} </span> 
             {/* <MusicNoteIcon fontSize='small' style={{marginLeft: 3}} /> */}
-            </span>
             </h3>
             {/* <p>{song.genres?.slice(1).join(', ')}</p> */}
             </Paper>
