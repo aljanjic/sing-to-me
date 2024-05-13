e7aa36280839c28594321e1f7739184a89bfc99e
Aljosa <janjic_aljosa@hotmail.com>
2024-05-13 14:00:21 +0200

    Adding TextField to application

--- src/pages/songs/index.tsx
+++ src/pages/songs/index.tsx
@@ -8,6 +8,7 @@ import SongComponent from "@/components/SongComponent";
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import React from "react";
+import TextField from "@mui/material/TextField";
 
 
 export type Song = {
@@ -54,22 +55,28 @@ const Songs: NextPage = ({ songs: s }: InferGetStaticPropsType<typeof getStaticP
             <Container>
 
 
-            <div className="relative flex flex-1 flex-shrink-0">
-                <label htmlFor="search" className="sr-only">
-                    Search    
-                </label> 
-                <input
-                    className="peer block w-1/2 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                    placeholder="search"
-                    onChange={(e) => {
-                        handleSearch(e.target.value);
-                    }}
-                />
-                {/* <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />  */}
-            </div>
 
-            <h1>Muzički žanr:</h1>
-                <Grid container alignItems="center" justifyContent="center">
+
+            {/* <h1>Muzički žanr:</h1> */}
+                <Grid container alignItems="center" justifyContent="center" sx={{ mt: 2}} >
+                    <Grid item>
+                        <TextField fullWidth label="Trazi pjesmu" id="fullWidth"                                 onChange={(e) => {
+                                    handleSearch(e.target.value);
+                                }} />
+
+{/* 
+                            <label htmlFor="search" className="sr-only">
+                                Search    
+                            </label> 
+                            <input
+                                className="peer block w-1/2 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                placeholder="search"
+                                onChange={(e) => {
+                                    handleSearch(e.target.value);
+                                }}
+                                /> */}
+                            {/* <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />  */}
+                    </Grid>
                     <Grid item>
                         {genres.map((genre: string, index: number) => {
                             return (
