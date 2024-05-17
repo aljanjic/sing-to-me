60c12bc97c8c28efa3e42367971fed491fa24c3f
Aljosa <janjic_aljosa@hotmail.com>
2024-05-17 16:29:03 +0200

    Trying new filter again

--- src/pages/songs/index.tsx
+++ src/pages/songs/index.tsx
@@ -75,16 +75,18 @@ const Songs: NextPage = ({ songs: s }: InferGetStaticPropsType<typeof getStaticP
       };
 
       const filteredSongs = songs.filter((song: Song) => {
-        const searchNormalized = normalizeText(searchSong);        
+        const searchNormalized = normalizeText(searchSong);
         if (searchNormalized) {
-            return normalizeText(song.songName.toLowerCase())?.includes(searchNormalized.toLowerCase()) || normalizeText(song.musician.toLowerCase())?.includes(searchNormalized.toLowerCase());
+            const songNameMatch = song.songName && typeof song.songName === 'string' &&
+                normalizeText(song.songName.toLowerCase()).includes(searchNormalized);
+            const musicianMatch = song.musician && typeof song.musician === 'string' &&
+                normalizeText(song.musician.toLowerCase()).includes(searchNormalized);
+    
+            return songNameMatch || musicianMatch;
         }
-        return activeGenre === 'All' || song.genres?.includes(activeGenre);
+        return activeGenre === 'All' || (song.genres && song.genres.includes(activeGenre));
     });
 
-
-
-    
     return (
         <>
             <Container>
